9347cf3ea2b1e349c732640ed3088fc033dba5da
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-02-21 22:13:25 +0200

    add jade view engine on express

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 var server = app.listen(app.get('port'), function() {
   console.log('App listening at http://localhost:%s/', app.get('port'));
 });
+
+app.set('views', path.join('__dirname', 'views'));
+app.set('view engine', 'jade');
+
+// app.use(express.static('public'));
