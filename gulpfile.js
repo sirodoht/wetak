77225e07cd4132dbb0d6995e1829c4af73a16260
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-02-21 23:27:11 +0200

    move app.js into back directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var server = require('gulp-express');
 
 gulp.task('serve', function() {
-  server.run(['app.js']);
+  server.run(['back/app.js']);
 });
 
 gulp.task('default', ['serve']);
