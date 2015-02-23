7d030b26e62aa0136e08231b14755ed8bcdd0016
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-02-23 03:43:11 +0200

    use body-parser to parse the POSTed json

--- back/app.js
+++ back/app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post('/parse', function(req, res) {
+  res.send(req.body);
+  console.log(req.body.dataString);
 });
 
 var server = app.listen(app.get('port'), function() {
